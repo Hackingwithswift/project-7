33e1fda269bcfaed92eaacfff64dc6dfce573419
veerababum <veerababum@users.noreply.github.com>
2019-10-31 16:59:30 -0700

    Parsing JSON using the Codable protocol

---
+++ CodableProtocal/CodableProtocal/petition.swift
@@ -0,0 +1,15 @@
+//
+//  petition.swift
+//  CodableProtocal
+//
+//  Created by vmulugu on 10/31/19.
+//  Copyright © 2019 Veerababu Mulugu. All rights reserved.
+//
+
+import Foundation
+struct Petition : Codable  {
+    var title = String()
+    var body = String()
+    var signatureCount = Int()
+
+}

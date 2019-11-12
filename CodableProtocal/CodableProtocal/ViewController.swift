5449a78013be728744038fa35585e4c2020f3c19
veerababum <veerababum@users.noreply.github.com>
2019-11-12 01:16:13 -0800

    GCD used by Performselector

--- CodableProtocal/CodableProtocal/ViewController.swift
+++ CodableProtocal/CodableProtocal/ViewController.swift
@@ -15,67 +15,46 @@ class ViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
         navigationController?.title = "Codable"
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
-        DispatchQueue.global(qos: .userInitiated).async {
-            if  let url = URL(string: urlString) {
-                print("the string is:\(url))")
-                if let data = try? Data(contentsOf: url) {
-                    // wew work here
-                    self.parse(json: data)
-                    
-                }
-            }
-        }
-        
-        
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
         
     }
-    func parse(json: Data){
+    @objc func fetchJSON(){
+        
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        if  let url = URL(string: urlString) {
+            print("the string is:\(url))")
+            if let data = try? Data(contentsOf: url) {
+                // wew work here
+                parse(json: data)
+                
+            }
+        }
+
+    }
+    
+    @objc func parse(json: Data){
         let decoder = JSONDecoder()
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
+            tableView.performSelector(onMainThread: #selector(tableView.reloadData), with: nil, waitUntilDone: false)
+        }else {
+                    performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
         }
-        showError()
+        
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // return petitions.count
         print(petitions.count)
         return petitions.count
     }
-    
-    //- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
-    
-    
-    //   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    //        let cell = tableView.dequeueReusableCell(withIdentifier: "NavController", for: indexPath)
-    //        cell.textLabel?.text = "Title goes here"
-    //        cell.detailTextLabel?.text = "Subtitle goes here"
-    //        return cell
-    //    }
-    //
-    
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        
         let petition = petitions[indexPath.row]
-        
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
     }
-    //    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-    //        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-    //        cell.textLabel?.text = "Title goes here"
-    //        cell.detailTextLabel?.text = "Subtitle goes here"
-    //        return cell
-    //
-    //    }
     
     func tableView(tableView: UITableViewDelegate, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
         print("wananananaanan" )
@@ -83,7 +62,6 @@ class ViewController: UITableViewController {
         
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         
         print("Did select method is called")
         let vc  = DetailViewController()
@@ -94,12 +72,11 @@ class ViewController: UITableViewController {
         
     }
     
-    func showError() {
-        DispatchQueue.main.async {
-            let ac = UIAlertController(title: "Loading Error", message: "There eas a problem loading the feed; please check your conncetion and try again", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            self.present(ac, animated: true)
-            
-        }
+    @objc func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
     }
+    
 }
